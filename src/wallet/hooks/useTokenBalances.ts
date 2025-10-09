83bdb6cdfb8576d11b7673f5332741198e93296b
郭维 <guodawei1021@gmail.com>
2025-10-09 08:59:34 +0800

    feat: trading 合约支持 getBalances method

--- src/wallet/hooks/useTokenBalances.ts
+++ src/wallet/hooks/useTokenBalances.ts
@@ -1,35 +1,59 @@
 import { useCallback } from "react";
 import { useClient } from "./useClient";
 import { Address, erc20Abi } from "viem";
+import { useTradingContract } from "../../contract";
 
 export function useTokenBalances() {
-  const { publicClient } = useClient()
+  const { publicClient } = useClient();
+  const tradingContract = useTradingContract();
 
-  const getTokenBalances = useCallback(async (account: Address, tokens: Address[]) => {
-    if (publicClient) {
-      try {
-        const result = await publicClient.multicall({
-          contracts: tokens.map((token) => ({
-            address: token,
-            abi: erc20Abi,
-            functionName: "balanceOf",
-            args: [account]
-          }))
-        })
+  const getTokenBalances = useCallback(
+    async (account: Address, tokens: Address[]) => {
+      if (publicClient) {
+        try {
+          const result = await publicClient.multicall({
+            contracts: tokens.map((token) => ({
+              address: token,
+              abi: erc20Abi,
+              functionName: "balanceOf",
+              args: [account],
+            })),
+          });
 
-        return result.map((r, i) => ({
-          token: tokens[i],
-          balance: r.result // bigint
-        }))
-      } catch(error) {
-        console.log(error)
-        return []
+          return result.map((r, i) => ({
+            token: tokens[i],
+            balance: r.result, // bigint
+          }));
+        } catch (error) {
+          console.log(error);
+          return [];
+        }
       }
-    }
-    return []
-  }, [publicClient])
+      return [];
+    },
+    [publicClient]
+  );
+
+  const getTokenBalancesByTradingContract = useCallback(
+    async (account: Address, tokens: Address[]) => {
+      if (publicClient && tradingContract) {
+        try {
+          const result = await tradingContract.read.getBalances([
+            account,
+            tokens,
+          ]);
+          return result;
+        } catch (error) {
+          console.log("getTokenBalancesByTradingContract error", error);
+          return [];
+        }
+      }
+    },
+    [publicClient]
+  );
 
   return {
-    getTokenBalances
-  }
+    getTokenBalances,
+    getTokenBalancesByTradingContract,
+  };
 }
